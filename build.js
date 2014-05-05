54e7a573a796073e227276e1f41ecbd3b79c8d72
Michiel@unhosted <michiel@unhosted.org>
2014-05-05 08:15:19 +0200

    correct page breaks

--- build.js
+++ build.js
@@ -2,16 +2,15 @@ var fs = require('fs'),
   lines = fs.readFileSync('source.txt').toString().split('\n'),
   breaker1a = '\n\nde Jong                                                         [Page ',
   breaker1b = '\n\nde Jong                                                        [Page ',
-  breaker2 = ']\n'
-    +'\n'
-    +'Internet-Draft              remoteStorage                  December 2013\n\n\n',
+  breaker2 = ']',
+  breaker3 = '\n\nInternet-Draft              remoteStorage                  December 2013\n\n',
   page = 1, line = 0;
 
 while (line < lines.length) {
   console.log(lines[line]);
   line++;
   if (line > 7 && line%43 === 7) {
-    console.log((page < 10 ? breaker1a : breaker1b) + page + breaker2);
+    console.log((page < 10 ? breaker1a : breaker1b) + page + breaker2 + (line < lines.length ? breaker3 : ''));
     page++;
   }
 }
