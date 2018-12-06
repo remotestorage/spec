8f291f471d89b7d8b7cd2f5aa40c6248a22b9a5b
Michiel de Jong <michiel@unhosted.org>
2018-12-06 17:38:09 +0100

    automate build script some more

--- build.js
+++ build.js
@@ -1,12 +1,33 @@
+const version = 11;
+const day = 7;
+const expiresDay = 8;
+
+if (version % 2 === 0) {
+  month = 'December';
+  expiresMonth = 'June';
+} else {
+  month = 'June';
+  expiresMonth = 'December';
+}
+const year = Math.floor(2013 + (version - 1) / 2);
+const expiresYear = Math.floor(2013 + version / 2);
+const date = `${day} ${month} ${year}`;
+const expires = `${expiresDay} ${expiresMonth} ${expiresYear}`;
+const monthYear = (month  === 'June' ? '    ' : '') + month + ' ' + year;
+
 var fs = require('fs'),
   lines = fs.readFileSync('source.txt').toString().split('\n'),
   breaker1a = '\n\nde Jong                                                         [Page ',
   breaker1b = '\n\nde Jong                                                        [Page ',
   breaker2 = ']',
-  breaker3 = '\n\nInternet-Draft              remoteStorage                      June 2018\n\n',
+  breaker3 = '\n\nInternet-Draft              remoteStorage                  '+ monthYear + '\n\n',
   page = 1, line = 0;
 
 while (line < lines.length) {
+  lines[line] = lines[line].split('${EXPIRES}').join(expires);
+  lines[line] = lines[line].split('${DATE}').join(date);
+  lines[line] = lines[line].split('${YEAR}').join(year);
+  lines[line] = lines[line].split('${VERSION}').join(version);
   console.log(lines[line]);
   if (lines[line].length > 72) {
     console.log('123456789012345678901234567890123456789012345678901234567890123456789012*****');
